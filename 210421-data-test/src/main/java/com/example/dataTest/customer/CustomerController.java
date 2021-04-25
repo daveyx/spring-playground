e3e70ffa9f5d1ad3ef698173e21f929aa35898a6
David <d.rehle@gmail.com>
2021-04-25 02:13:39 +0200

    save customer

--- 210421-data-test/src/main/java/com/example/dataTest/customer/CustomerController.java
+++ 210421-data-test/src/main/java/com/example/dataTest/customer/CustomerController.java
@@ -1,10 +1,7 @@
 package com.example.dataTest.customer;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,8 +16,13 @@ public class CustomerController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Customer> getCustomer(@PathVariable("id") final Long id) {
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("id") final Long id) {
         return ResponseEntity.ok(customerService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer customer) {
+        return ResponseEntity.ok(customerService.save(customer));
     }
 
 }
--- 210421-data-test/src/test/java/com/example/dataTest/customer/CustomerControllerIntegrationTest.java
+++ 210421-data-test/src/test/java/com/example/dataTest/customer/CustomerControllerIntegrationTest.java
@@ -1,54 +1,72 @@
 package com.example.dataTest.customer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
-@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @SpringBootTest
 @AutoConfigureMockMvc
+@EnableWebMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CustomerControllerIntegrationTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Autowired
-    private CustomerRepository customerRepository;
-
-    @Autowired
-    private CustomerService customerService;
-
-    @Autowired
     private ObjectMapper objectMapper;
-
-    private Customer customer;
 
 
     @BeforeEach
     public void beforeEach() {
-        customer = customerRepository.save(new Customer("DareIT", "LLC"));
-        customer = customerService.save(new Customer("DareIT", "LLC"));
     }
 
     @Test
-    void testAddPerson() throws Exception {
+    @Order(1)
+    void testSaveCustomer() throws Exception {
+        Customer customer = createCustomer();
+        MvcResult mvcResult = mvc.perform(post("/customer")
+                .content(objectMapper.writeValueAsString(customer))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String json = mvcResult.getResponse().getContentAsString();
+
+        customer.setId(1L);
+
+        assertEquals(objectMapper.writeValueAsString(customer), json);
+    }
+
+    @Test
+    @Order(2)
+    void testGetCustomerById() throws Exception {
         MvcResult mvcResult
-                = mvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/customer/" + customer.getId())).andReturn();
+                = mvc.perform(get("/customer/1")).andReturn();
 
         String json = mvcResult.getResponse().getContentAsString();
 
 //        does not work, as java.lang.Object.equals is called
 //        assertEquals(customer, objectMapper.readValue(json, Customer.class));
 
+        Customer customer = createCustomer();
+        customer.setId(1L);
+
         assertEquals(objectMapper.writeValueAsString(customer), json);
+    }
+
+    private Customer createCustomer() {
+        return new Customer("DareIT", "LLC");
     }
 
 }
