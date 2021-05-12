998a954cc5040603a303f0e642efc5fdeb4fbaf2
David <d.rehle@gmail.com>
2021-05-12 03:34:07 +0200

    use lombok

--- 210427-datatest/src/main/java/com/example/datatest/Entity.java
+++ 210427-datatest/src/main/java/com/example/datatest/Entity.java
@@ -1,6 +1,6 @@
 package com.example.datatest;
 
-
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,28 +9,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
+@Getter
+@Setter
 @javax.persistence.Entity
 public class Entity {
 
+    @Setter(AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String data;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
 }
--- 210427-datatest/src/test/java/com/example/datatest/ControllerIntegrationTest.java
+++ 210427-datatest/src/test/java/com/example/datatest/ControllerIntegrationTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
-@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ControllerIntegrationTest {
@@ -36,9 +36,8 @@ public class ControllerIntegrationTest {
     }
 
     @Test
-    void test() throws Exception {
-        String actual
-                = mockMvc.perform(get("/controller/1")).andReturn().getResponse().getContentAsString();
+    void test_equals() throws Exception {
+        String actual = mockMvc.perform(get("/controller/1")).andReturn().getResponse().getContentAsString();
 
         assertEquals(actual, createExpected());
     }
@@ -46,7 +45,7 @@ public class ControllerIntegrationTest {
     private String createExpected() throws JsonProcessingException {
         Entity entity = new Entity();
         entity.setData("data in hsqldb");
-        entity.setId(1L);
+        ReflectionTestUtils.setField(entity, "id", 1L);
         return objectMapper.writeValueAsString(entity);
     }
 
