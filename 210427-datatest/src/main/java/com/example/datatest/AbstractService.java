e38a8399ef0aa65e7564a0fe152cc7b52b7e3323
David <d.rehle@gmail.com>
2021-05-20 03:16:30 +0200

    use correct @Transactional

--- 210427-datatest/src/main/java/com/example/datatest/AbstractService.java
+++ 210427-datatest/src/main/java/com/example/datatest/AbstractService.java
@@ -2,8 +2,8 @@ package com.example.datatest;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 
